8bfc88cb5220113eb86ad6202474821c097b1042
grant <grantpeltier93@gmail.com>
2014-09-18 00:07:58 -0500

    added tic and tac classes

---
+++ Tac.java
@@ -0,0 +1,12 @@
+package Assignment1_360C;
+
+public class Tac {
+	//Attributes
+	private int id;
+	private int weight;
+	
+	//Constructors
+			
+			
+	//Getters & Setters
+}
---
+++ Tic.java
@@ -0,0 +1,15 @@
+package Assignment1_360C;
+
+public class Tic {
+		//Attributes
+		private int id;
+		private int min;
+		private int max;
+		private int weight;
+		
+		//Constructors
+		
+		
+		//Getters & Setters
+	
+}
