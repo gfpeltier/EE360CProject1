71bbfdeb39cc4c4ead39f9ce5749bf1ae2524bdb
grant <grantpeltier93@gmail.com>
2014-09-28 16:52:55 -0500

    added edge class and began input parsing for driver

---
+++ Edge.java
@@ -0,0 +1,18 @@
+package Assignment1_360C;
+
+public class Edge {
+	
+	//attributes
+	private int tac;
+	private int tic;
+	private int weight;
+	
+	//constructors
+	public Edge(int tacID, int ticID, int edgeWeight){
+		tac = tacID;
+		tic = ticID;
+		weight = edgeWeight;
+	}
+	
+	//methods
+}
--- HW1_Driver.java
+++ HW1_Driver.java
@@ -1,13 +1,64 @@
 package Assignment1_360C;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+
 public class HW1_Driver {
+	
+	Tic[] GraphTics;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if (args.length != 2) 
+		{
+			System.err.println ("Error: Incorrect number of command line arguments");
+			System.exit(-1);
+		}
+		File input = new File(args[0]);
+		File output = new File(args[1]);
+		
+		try{
+			BufferedReader plain = new BufferedReader(new FileReader(input));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+			
+			int lineIndex = 0;
+			int numGraphs = 0;
+			for (String s = plain.readLine(); s != null; s = plain.readLine()){
+				if(lineIndex == 0){
+					numGraphs = Integer.parseInt(s);
+				}
+				for(int k = 0; k < s.length(); k++){
+					char reg = s.charAt(k);
+					String num = "";
+					if(reg != ' '){
+						num += reg;
+					}else{
+						
+					}
+				}
+				lineIndex++;
+			}
+			writer.close();
+		}catch (FileNotFoundException e) {
+            // File not found
+            e.printStackTrace();
+
+		}catch (IOException e) {
+            // Not able to read line
+            e.printStackTrace();
+      }
 
 	}
+	
+	
 
 }
